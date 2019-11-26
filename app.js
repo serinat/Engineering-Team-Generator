0b77092f13bac83178e65b8af85d072b8b1df2b9
Serina Tsang <serinat@uw.edu>
2019-11-26 11:17:17 -0800

    added questions.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const questions = require("./lib/Questions");
 
 function init() {
     inquirer
